1e62919644018f873eaedf4151ae8085af47c393
keipp <random>
2021-11-23 21:30:05 +0000

    Pievienoju žurnalēšanu

--- NumberGuessing.py
+++ NumberGuessing.py
@@ -47,3 +47,11 @@ cur.execute('CREATE TABLE IF NOT EXISTS result ([player_name] VARCHAR[15], [atte
 cur.execute('INSERT INTO result (player_name, attempts) VALUES (?, ?)', (name1, min(attempts_list)))
 con.commit()
 con.close()
+logging.basicConfig(
+    level=logging.INFO,
+    format='{asctime} {message}',
+    style='{',
+    filename='mylog.log',
+    filemode='w'
+)
+logging.info("%s played the number guessing game" %name1)
